52d7a24beb29230b3e93c96c25fd49fd28f31df6
Lorenzo Cazzato <120551403+lorenzocazzato@users.noreply.github.com>
2023-07-13 16:03:34 +0200

    Add files via upload

---
+++ es9/exercise.js
@@ -0,0 +1,9 @@
+const person = {
+  firstName: "Mario",
+  lastName: "Rossi",
+  age: 25,
+};
+Object.keys(person).forEach((key) => {
+  console.log(`${key}: ${person[key]}`);
+});
+// Print values of person using Object.keys
